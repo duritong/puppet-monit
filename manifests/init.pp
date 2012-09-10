41f08e2be1da7fbcec5e02c1754fa77a25f9b27a
mh <mh@immerda.ch>
2012-09-11 00:25:43 +0200

    migrate to parametrized classes
    Instead of using global variables.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,61 +6,60 @@
 # Stig Sandbeck Mathisen <ssm@fnord.no>
 # Micah Anderson micah@riseup.net
 #
-# To set any of the following, simply set them as variables in your manifests
+# To set any of the following, simply set them as class parameters
 # before the class is included, for example:
 #
-# $monit_enable_httpd = yes
-# $monit_httpd_port = 8888
-# $monit_secret="something secret, something safe"
-# $monit_alert="someone@example.org"
-# $monit_mailserver="mail.example.org"
-# $monit_pool_interval="120"
-# 
-# include monit
+# class{'monit':
+#   alert => 'someone@example.org',
+#   mailserver => 'mail.example.com'
+# }
 #
 # The following is a list of the currently available variables:
 #
-# monit_alert:                who should get the email notifications?
-#                             Default: root@localhost
+# alert:         who should get the email notifications?
+#                Default: root@localhost
 #
-# monit_enable_httpd:         should the httpd daemon be enabled?
-#                             set this to 'yes' to enable it, be sure
-#                             you have set the $monit_default_secret
-#                             Valid values: yes or no
-#                             Default: no
+# enable_httpd:  should the httpd daemon be enabled?
+#                set this to 'yes' to enable it, be sure
+#                you have set the $monit_default_secret
+#                Valid values: yes or no
+#                Default: no
 #
-# monit_httpd_port:           what port should the httpd run on?
-#                             Default: 2812
+# httpd_port:    what port should the httpd run on?
+#                Default: 2812
 #
 #
-# monit_mailserver:           where should monit be sending mail?
-#                             set this to the mailserver
-#                             Default: localhost
+# mailserver:    where should monit be sending mail?
+#                set this to the mailserver
+#                Default: localhost
 #
-# monit_pool_interval:        how often (in seconds) should monit poll?
-#                             Default: 120
+# pool_interval: how often (in seconds) should monit poll?
+#                Default: 120
 #
-
-class monit {
-
-
+# secret:        The secret for the httpd daemon. Please set it!
+#                Default: "This is not very secret, is it?"
+#
+class monit(
 	# The monit_secret is used with the fqdn of the host to make a
 	# password for the monit http server.
-	$monit_default_secret="This is not very secret, is it?"
+	$secret = 'This is not very secret, is it?',
 
-	# The default alert recipient.  You can override this by setting the
-	# variable "$monit_alert" in your node specification.
-	$monit_default_alert="root@localhost"
+	# The default alert recipient.
+	$alert = 'root@localhost',
 
-        # How often should the daemon pool? Interval in seconds.
-        case $monit_pool_interval {
-          '': { $monit_pool_interval = '120' }
-        }
+  # How often should the daemon pool? Interval in seconds.
+  $pool_interval = '120',
 
-        # Should the httpd daemon be enabled, or not? By default it is not
-        case $monit_enable_httpd {
-          '': { $monit_enable_httpd = 'no' }
-        }
+  # Should the httpd daemon be enabled, or not? By default it is not
+  $enable_httpd = 'no',
+
+  # port for the httpd daemon
+  $httpd_port = 2812,
+
+  # mailserver
+  $mailserver = 'localhost'
+){
+
 	# The package
 	package { "monit":
 		ensure => installed,
@@ -110,7 +109,7 @@ class monit {
 	case $operatingsystem {
 		"debian": {
 			file { "/etc/default/monit":
-				content => "startup=1\nCHECK_INTERVALS=${monit_pool_interval}\n",
+				content => "startup=1\nCHECK_INTERVALS=${monit::pool_interval}\n",
 				before  => Service["monit"]
 			}
 		}
